7bff6e132dbd576c90f0c59cfb4fe8b027bf61ac
aninditasarkar678@gmail.com <aninditasarkar678@gmail.com>
2025-02-21 14:16:18 +0530

    Creating objects

--- first.js
+++ first.js
@@ -1,1 +1,12 @@
-console.log("Anindita Sarkar");
+
+//Object
+const student = {
+    name: 'Anindita Chaturbedi',
+    age: 21,
+    cgpa:8.8,
+    isStudent: true
+};
+
+const name = "Anindita Chaturbedi";
+name = "Anindita";
+console.log(student["name"]);
