b4e2c50f9c84c99db1af4d27608c418661f363f6
aninditasarkar678@gmail.com <aninditasarkar678@gmail.com>
2025-02-23 01:02:27 +0530

    sorted

--- first.js
+++ first.js
@@ -29,3 +29,72 @@ b= ++a;
 console.log("a =",a, "&b =",b);
 */
 
+// odd and even no find
+
+/*let no= 7;
+if(no % 2 ==0){
+    console.log("Even");
+}else{
+    console.log("Odd");
+}
+*/
+//find smallest no
+/*
+let num =2, num1 =4;
+if(num<num1){
+    console.log("num is smallest");
+}else{
+    console.log("num1 is smallest");
+}
+
+//syntax -> Rules
+
+
+
+//Ternary Operator
+let age =98;
+console.log(age>20 ?"elligible":"not"
+);
+
+*/
+
+// Switch case
+
+/*let name = "bitan";
+switch(name){
+    case "ana":
+        console.log("Gold medal owner");
+        break;
+    case "bikan":
+        console.log("Silver medal owner");
+        break;
+    case "chiku":
+        console.log("Bronze medal owner");
+        break;
+    default:
+        console.log("Don't have any one of them!");
+}*/
+
+/*let num = prompt("Enter the number: ");
+if (num%5 ===0){
+    console.log(num,"is multiple of 5");
+}else{
+    console.log(num,"is not multiple of 5");
+}
+*/
+
+let score =prompt("The score is: ");
+if(score>=90 && score<=100){
+    console.log("Grade A");
+}else if(score>=70 && score<=89){
+    console.log("Grade B");
+}else if(score>=60 && score<=69){
+    console.log("Grade C");
+}
+else if(score>=50 && score<=59){
+    console.log("Grade D");
+}
+else if(score>=0 && score<=49)
+{
+    console.log("Grade F");
+}
