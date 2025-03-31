e75a90d24b88b6942c66ec94aa1df3ea6acfdaae
aninditasarkar678@gmail.com <aninditasarkar678@gmail.com>
2025-03-31 21:07:13 +0530

    solved upto reduce method

--- first.js
+++ first.js
@@ -367,3 +367,39 @@ console.log(new_Companies);
 
 // Revision
 
+/*
+function countVowels(str){
+    let count =0;
+    for(let char of str){
+        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
+            char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U'){
+            count++;
+        }
+    }
+    
+    console.log("Total vowels are:",count);
+    
+    return count;
+}
+
+const ArrowcountVowels = (str) => {
+    let count =0;
+    for(let char of str){
+        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
+            char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U'){
+            count++;
+        }
+    }
+    
+    console.log("Total vowels are now :",count);
+    
+    return count;
+}
+    */
+
+let arr = [1,2,10];
+
+let multi = arr.reduce((prev, cur ) => 
+    prev * cur
+);
+console.log(multi);
